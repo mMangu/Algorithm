5b202d39b307753333e77e58f1c0b106a3bb4385
mMangu <alsrb4298@naver.com>
2021-03-09 21:44:02 +0900

    backtracking

---
+++ algorithm/backtracking/9663.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int n, ans, check[16];
+
+bool Possible(int x) {
+    for (int i = 0; i < x; i++)
+        if (check[i] == check[x] || abs(check[i] - check[x]) == x - i) return false;
+    return true;
+}
+
+void Solve(int x) {
+    if (x == n) {
+        ans++;
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        check[x] = i;
+        if (Possible(x)) Solve(x + 1);
+    }
+}
+
+int main() {
+    scanf("%d", &n);
+    Solve(0);
+    printf("%d", ans);
+}
