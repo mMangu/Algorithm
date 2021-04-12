2be8105ccdd4a41d677add8410309ab0681415f7
mMangu <alsrb4298@naver.com>
2021-04-13 08:25:22 +0900

    dp + dfs

--- algorithm/dp/1520.cpp
+++ algorithm/dp/1520.cpp
@@ -1,25 +1,29 @@
-//dfs TLE 코드
 #include <cstdio>
 
-int map[502][502], N, M, ans;
+int map[502][502], dp[502][502], N, M;
+int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-void dfs(int i, int j) {
-    if (i < 0 || i >= M || j < 0 || j >= N) return;
-    if (i == M - 1 && j == N - 1) {
-        ans++;
-        return;
+int dfs(int y, int x) {
+    if (y == M - 1 && x == N - 1) return 1;
+    if (dp[y][x] != -1) return dp[y][x];
+    dp[y][x] = 0;
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dir[i][0], ny = y + dir[i][1];
+        if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+            if (map[y][x] > map[ny][nx]) dp[y][x] += dfs(ny, nx);
+        }
     }
-    if (map[i][j] > map[i][j + 1]) dfs(i, j + 1);
-    if (map[i][j] > map[i + 1][j]) dfs(i + 1, j);
-    if (map[i][j] > map[i][j - 1]) dfs(i, j - 1);
-    if (map[i][j] > map[i - 1][j]) dfs(i - 1, j);
+    return dp[y][x];
 }
 
 int main() {
     scanf("%d %d", &M, &N);
-    for (int i = 0; i < M; i++) 
-        for (int j = 0; j < N; j++) 
+    for (int i = 0; i < M; i++) { 
+        for (int j = 0; j < N; j++) {
             scanf("%d", &map[i][j]);
-    dfs(0, 0);
+            dp[i][j] = -1;
+        }
+    }
+    int ans = dfs(0, 0);
     printf("%d", ans);
 }
