e3445196793a8b5e21bad695ce0b6903d48354e8
mangu <66214527+mMangu@users.noreply.github.com>
2021-07-04 22:12:35 +0900

    RGB거리 2 - dp

---
+++ algorithm/dp/17404.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1002][3];
+int dp[1002][3];
+
+int main() {
+    int n; scanf("%d", &n);
+    for (int i = 1; i <= n; i++) 
+        for (int j = 0; j < 3; j++) 
+            scanf("%d", &arr[i][j]);
+
+    int ans = 1e9;
+    for (int k = 0; k < 3; k++) {
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+            dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+            dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+            if (i == 1) for (int j = 0; j < 3; j++) if (j != k) dp[i][j] = 1e9;
+            if (i == n) dp[i][k] = 1e9;
+        }
+        ans = min(ans, min(dp[n][0], min(dp[n][1], dp[n][2])));
+    }
+    printf("%d", ans);
+}
