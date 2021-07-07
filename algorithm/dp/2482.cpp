fe86facd65e2c662eb27c61ef2ae799c338c8cae
mangu <66214527+mMangu@users.noreply.github.com>
2021-07-08 01:15:21 +0900

    색상환 - dp

--- algorithm/dp/2482.cpp
+++ algorithm/dp/2482.cpp
@@ -8,7 +8,9 @@ const int mod = 1e9 + 3;
 
 int dpf(int cur, int cnt) {
     int &ret = dp[cur][cnt];
+    //cnt가 1이면 남은 것중에 아무거나 하나만 칠하면 됨
     if (cnt == 1) return cur;
+    //cur이 2 * cnt보다 작으면 인접하는 경우가 무조건 한개 이상 나옴
     if (cur <= 0 || cur < 2 * cnt) return 0;
     if (~ret) return ret;
     return ret = (dpf(cur - 2, cnt - 1) + dpf(cur - 1, cnt)) % mod;
