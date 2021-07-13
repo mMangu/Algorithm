0e023cb1d52fdfb29280e0da5390c82297a942a2
mangu <66214527+mMangu@users.noreply.github.com>
2021-07-13 16:04:28 +0900

    휴대폰 자판 - trie

--- algorithm/trie/5670.cpp
+++ algorithm/trie/5670.cpp
@@ -4,15 +4,15 @@
 struct Trie {
     bool finish;
     int child;
-    Trie *next[26];
+    Trie *nxt[26];
 
     Trie() : finish(false), child(0) {
-        memset(next, 0, sizeof(next));
+        memset(nxt, 0, sizeof(nxt));
     }
 
     ~Trie() {
         for (int i = 0; i < 26; i++) {
-            if (next[i]) delete next[i];
+            if (nxt[i]) delete nxt[i];
         }
     }
 
@@ -22,22 +22,23 @@ struct Trie {
             return;
         }
         int idx = *key - 'a';
-        if (!next[idx]) {
+        if (!nxt[idx]) {
             child++;
-            next[idx] = new Trie();
+            nxt[idx] = new Trie();
         }
-        next[idx] -> insert(key + 1);
+        nxt[idx] -> insert(key + 1);
     }
 
     int find(const char *key) {
         if (*key == 0) return 0;
         int idx = *key - 'a';
-        if (child > 1 || (child == 1 && finish)) return next[idx] -> find(key + 1) + 1;
-        else return next[idx] -> find(key + 1);
+        if (child > 1 || (child == 1 && finish)) return nxt[idx] -> find(key + 1) + 1;
+        else return nxt[idx] -> find(key + 1);
     }
 };
 
 char ipt[100005][81];
+
 int main() {
     int N;
     while (scanf("%d", &N) != EOF) {
