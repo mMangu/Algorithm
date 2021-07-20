0e74077656ff17d727c7abd0818c97c6a4da8d48
mangu <66214527+mMangu@users.noreply.github.com>
2021-07-21 00:32:44 +0900

    최종 순위 - 위상 정렬

--- algorithm/topological_sort/3665.cpp
+++ algorithm/topological_sort/3665.cpp
@@ -7,12 +7,12 @@ using namespace std;
 vector<int> v;
 int g[555][555];
 
-void solve(){
+void solve() {
 	memset(g, 0, sizeof g);
 	int n; cin >> n; v.resize(n);
 	for (auto &i : v) cin >> i;
-	for (int i = 0; i < n; i++){
-		for (int j = i + 1; j < n; j++){
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
 			g[v[i]][v[j]] = 1;
 		}
 	}
@@ -22,15 +22,15 @@ void solve(){
 		swap(g[a][b], g[b][a]);
 	}
 
-	for (int i = 0; i < n; i++){
-		for (int j = i + 1; j < n; j++){
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
 			if (g[v[j]][v[i]]) swap(v[i], v[j]);
 		}
 	}
 
-	for (int i = 0; i < n; i++){
-		for (int j = i + 1; j < n; j++){
-			if (g[v[j]][v[i]]){
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (g[v[j]][v[i]]) {
 				cout << "IMPOSSIBLE\n";
                 return;
 			}
